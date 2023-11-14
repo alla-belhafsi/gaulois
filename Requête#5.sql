4fd2b711216d4c5da4934132d829e61414c2c98e
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-14 16:56:08 +0100

    In Progress

--- Requête#5.sql
+++ Requête#5.sql
@@ -6,7 +6,7 @@ SELECT
 FROM
     bataille
 LEFT JOIN
-    lieu ON bataille.id_lieu = lieu.id_lieu -- Joindre la table "lieu" et "bataille" sur leur relation d'ID
+    lieu ON bataille.id_lieu = lieu.id_lieu -- Joindre la table "bataille" et "lieu" sur leur relation d'ID
 GROUP BY
     bataille.nom_bataille, bataille.date_bataille, lieu.nom_lieu -- Regroupement des résultats par nom de la bataille, date de la bataille et lieu de la bataille
 ORDER BY

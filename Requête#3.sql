e6845f73cdfc22374713980954901e8376a3b0d3
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-14 17:29:47 +0100

    Completed

--- Requête#3.sql
+++ Requête#3.sql
@@ -1,35 +1,45 @@
--- Requête 1: Sélectionne les informations sur les personnages, leurs adresses, les spécialités et les lieux, triés par nom du personnage
+-- Requête 1: triés par nom du personnage
 
+-- Sélectionne le nom du personnage depuis la table "personnage" en l'aliasant en "nom_personnage", le nom de la spécialité depuis la table "specialite",l'adresse du personnage depuis la table "personnage" et le nom du lieu depuis la table "lieu"
 SELECT 
-    personnage.nom_personnage AS nom_personnage, -- Sélectionne le nom du personnage depuis la table "personnage" en l'aliasant en "nom_personnage"
-    specialite.nom_specialite, -- Sélectionne le nom de la spécialité depuis la table "specialite"
-    personnage.adresse_personnage, -- Sélectionne l'adresse du personnage depuis la table "personnage"
-    lieu.nom_lieu -- Sélectionne le nom du lieu depuis la table "lieu"
+    personnage.nom_personnage AS nom_personnage, 
+    specialite.nom_specialite, 
+    personnage.adresse_personnage, 
+    lieu.nom_lieu 
 FROM 
-    lieu
-LEFT JOIN 
-    personnage ON lieu.id_lieu = personnage.id_personnage -- Joindre la table "lieu" et "personnage" sur leur relation d'ID
-LEFT JOIN 
-    specialite ON lieu.id_lieu = specialite.id_specialite -- Joindre la table "lieu" et "specialite" sur leur relation d'ID
+    personnage
+-- Joindre les informations de la table "personnage" avec la table "lieu" en utilisant les identifiants d'emplacement
+INNER JOIN 
+    lieu ON personnage.id_lieu = lieu.id_lieu
+-- Joindre les informations de la table "personnage" avec la table "specialite" en utilisant les identifiants de spécialité
+INNER JOIN 
+    specialite ON personnage.id_specialite = specialite.id_specialite
+-- Regroupement des résultats par nom du personnage, nom de spécialité, adresse du personnage et nom du lieu
 GROUP BY 
-    personnage.nom_personnage, specialite.nom_specialite, personnage.adresse_personnage, lieu.nom_lieu -- Regroupement des résultats par nom du personnage, nom de spécialité, adresse du personnage et nom du lieu
+    personnage.nom_personnage, specialite.nom_specialite, personnage.adresse_personnage, lieu.nom_lieu
+-- Trie les résultats par nom du personnage
 ORDER BY 
-    personnage.nom_personnage; -- Trie les résultats par nom du personnage
+    personnage.nom_personnage; 
 
--- Requête 2: Sélectionne les informations sur les lieux, les adresses des personnages, les noms des personnages et les spécialités, triés par nom du lieu
+-- Requête 2: triés par nom du lieu
 
+-- Sélectionne le nom du lieu depuis la table "lieu" et l'aliasant en "nom_lieu", l'adresse du personnage depuis la table "personnage",le nom du personnage depuis la table "personnage" et le nom de la spécialité depuis la table "specialite"
 SELECT 
-    lieu.nom_lieu AS nom_lieu, -- Sélectionne le nom du lieu depuis la table "lieu" et l'aliasant en "nom_lieu"
-    personnage.adresse_personnage, -- Sélectionne l'adresse du personnage depuis la table "personnage"
-    personnage.nom_personnage, -- Sélectionne le nom du personnage depuis la table "personnage"
-    specialite.nom_specialite -- Sélectionne le nom de la spécialité depuis la table "specialite"
+    lieu.nom_lieu AS nom_lieu,
+    personnage.adresse_personnage,
+    personnage.nom_personnage, 
+    specialite.nom_specialite 
 FROM 
-    lieu
-LEFT JOIN 
-    personnage ON lieu.id_lieu = personnage.id_personnage -- Joindre la table "lieu" et "personnage" sur leur relation d'ID
-LEFT JOIN 
-    specialite ON lieu.id_lieu = specialite.id_specialite -- Joindre la table "lieu" et "specialite" sur leur relation d'ID
+    personnage
+-- Joindre les informations de la table "personnage" avec la table "lieu" en utilisant les identifiants d'emplacement
+INNER JOIN 
+    lieu ON personnage.id_lieu = lieu.id_lieu
+-- Joindre les informations de la table "personnage" avec la table "specialite" en utilisant les identifiants de spécialité
+INNER JOIN 
+    specialite ON personnage.id_specialite = specialite.id_specialite
+-- Regroupement des résultats par nom du lieu, adresse du personnage, nom du personnage et nom de la spécialité 
 GROUP BY 
-    lieu.nom_lieu, personnage.adresse_personnage, personnage.nom_personnage, specialite.nom_specialite -- Regroupement des résultats par nom du lieu, adresse du personnage, nom du personnage et nom de la spécialité
+    lieu.nom_lieu, personnage.adresse_personnage, personnage.nom_personnage, specialite.nom_specialite
+-- Trie les résultats par nom du lieu
 ORDER BY 
-    lieu.nom_lieu; -- Trie les résultats par nom du lieu
+    lieu.nom_lieu; 

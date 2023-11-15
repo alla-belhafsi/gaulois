20496bf05089243750a4e9759d8187a2f7a492c6
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-15 10:17:21 +0100

    completed

--- Requête#6.sql
+++ Requête#6.sql
@@ -1,1 +1,13 @@
--- Sélectionne les "nom_potions"
+-- Pour chaque 'composer.id_potion' lier à 'composer.id_ingrédient' multiplié 'composer.qte' par 'ingredient.cout_ingredient'.
+-- Donc 'cout_potion' est l'additionnement des résultats de '(composer.qte)*('ingredient.cout_ingredient)' pour chaque 'composer.id_potion' lier à 'composer.id_ingrédient'
+-- La somme des coûts de chaque multiplication de lingrédient et sa quantité donne le coût total de chaque potion
+
+SELECT potion.nom_potion, 
+       SUM(composer.qte * ingredient.cout_ingredient) AS cout_potion
+FROM composer
+-- Jointure entre la table 'composer' et la table 'ingredient' sur leur relation d'ID d'ingrédient
+INNER JOIN ingredient ON composer.id_ingredient = ingredient.id_ingredient
+-- Jointure entre la table 'composer' et la table 'potion' sur leur relation d'ID d'ingrédient
+INNER JOIN potion ON composer.id_potion = potion.id_potion
+GROUP BY potion.nom_potion;
+

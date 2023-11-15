bed5073a88ebf8235acd6780a619b08ac96859ad
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-15 10:23:42 +0100

    completed

--- Requête#5.sql
+++ Requête#5.sql
@@ -1,13 +1,14 @@
 -- Sélectionne les informations sur les batailles, leurs nom, les dates et les lieux, triés par date
+SET lc_time_names = 'fr_FR';
 SELECT
-    bataille.date_bataille AS date_bataille, -- Sélectionne la date de la bataille depuis la table "bataille" en l'aliasant en "date_bataille"
+    DATE_FORMAT(bataille.date_bataille, '%W %e %M %Y') AS date_bataille, -- Sélectionne la date de la bataille depuis la table "bataille" en l'aliasant en "date_bataille"
     bataille.nom_bataille, -- Sélectionne le nom de la bataille depuis la table "bataille"
     lieu.nom_lieu -- Sélectionne le nom du lieu depuis la table "lieu"
 FROM
     bataille
 
 -- Joindre la table "bataille" et "lieu" sur leur relation d'ID
-LEFT JOIN
+INNER JOIN
     lieu ON bataille.id_lieu = lieu.id_lieu 
 
 -- Regroupement des résultats par nom de la bataille, date de la bataille et lieu de la bataille
@@ -16,4 +17,4 @@ GROUP BY
 
 -- Trie par date de la bataille dans l'ordre décroissant avec un format spécifique (année-mois-jour)
 ORDER BY
-    DATE_FORMAT(bataille.date_bataille, '%Y-%m-%d') DESC; 
+    bataille.date_bataille DESC; 

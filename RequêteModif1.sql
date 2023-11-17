77fdfd11b5cab91b942adee0663873c50b1c777b
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-17 09:29:51 +0100

    Fini

---
+++ RequêteModif1.sql
@@ -0,0 +1,15 @@
+--  Ajoutez le personnage suivant : Champdeblix, agriculteur (ID = 12), résidant à la ferme Hantassion de Rotomagus (ID = 6).
+
+-- Cette commande ajoute un nouveau personnage à la table 'personnage'.
+
+-- INSERT INTO est une commande SQL utilisée pour ajouter des données à une table.
+INSERT INTO personnage (nom_personnage, adresse_personnage, id_lieu, id_specialite)
+
+-- VALUES est utilisé avec INSERT INTO pour spécifier les valeurs à insérer dans la table.
+VALUES ('Champdeblix', 'Hantassion', 6, 12);
+
+
+
+
+
+
---
+++ RequêteModif2.sql
@@ -0,0 +1,19 @@
+
+-- Autorisez Bonemine à boire de la potion magique, elle est jalouse d'Iélosubmarine...
+
+-- INSERT INTO est une commande SQL utilisée pour ajouter des données à une table.
+INSERT INTO autoriser_boire (id_potion, id_personnage)
+
+-- VALUES est utilisé avec INSERT INTO pour spécifier les valeurs à insérer dans la table.
+VALUES (1, 12);
+
+-- Modifier la potion de Bonemine pour pouvoir boire de la potion magique, elle est jalouse d'Iélosubmarine...
+
+-- L'instruction UPDATE modifie les enregistrements dans une table.
+--UPDATE autoriser_boire
+
+-- La clause SET dans une instruction UPDATE indique quelles colonnes doivent être mises à jour et avec quelles valeurs.
+--SET id_potion = 13
+
+-- Ici, elle met à jour la table autoriser_boire en changeant l'ID de la potion pour Bonemine, qui a un ID de personnage égal à 12.
+--WHERE id_personnage = 12;

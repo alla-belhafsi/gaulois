1202151406cb75d2cd05e8d15262c7b8df8756cd
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-17 20:09:37 +0100

    Corrected !

--- RequêteModif3.sql
+++ RequêteModif3.sql
@@ -6,7 +6,7 @@ DELETE FROM casque
 -- Filtre pour obtenir les données des casques grecs
 WHERE 
 
--- Si l'ID des casques correspondant à 'id_type_casque = 2' n'est pas inclus dans la table 'prendre_casque'
+-- Si l'ID des casques correspondant à 'id_type_casque = 2' n'est pas inclus dans la table 'prendre_casque' alors ils n'ont jamais été pris lors d'une bataille
 AND id_casque NOT IN (
     SELECT id_casque
     FROM prendre_casque
